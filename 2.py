fd71cecd3f1b32c0e668bc2fff97646cbd9850e0
filatok <olenkaul@gmail.com>
2025-11-25 19:58:09 +0200

    for veronika

---
+++ 2.py
@@ -0,0 +1,11 @@
+def wordScore(s):
+    n=0
+    s = s.lower()
+    for i in s:
+        if i=='a' or i=='o' or i=='e' or i=='i' or i=='y':
+            n+=2
+        elif i=='m' or i=='r' or i=='g':
+            n+=3
+        else:
+            n+=1
+    return n
